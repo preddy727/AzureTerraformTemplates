e48b3990f587d95e36add3f3db4478068a825b95
preddy727 <47993564+preddy727@users.noreply.github.com>
2019-03-14 18:49:06 -0500

    Update main.tf

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -1,11 +1,17 @@
 data "azurerm_client_config" "current" {}
 
+data "azurerm_subnet" "terraform" {
+  name                 = "fillthisin"
+  virtual_network_name = "fillthisin"
+  resource_group_name  = "fillthisin"
+}
+
 resource "azurerm_resource_group" "vault_rg" {
   name     = "${var.name_prefix}-rg"
   location = "${var.location}"
 }
 
-resource "azurerm_key_vault" "vm_vault" {
+resource "azurerm_key_vault" "vmvault" {
   name                        = "${var.name_prefix}-vault"
   location                    = "${azurerm_resource_group.vault_rg.location}"
   resource_group_name         = "${azurerm_resource_group.vault_rg.name}"
@@ -15,11 +21,24 @@ resource "azurerm_key_vault" "vm_vault" {
   sku {
     name = "${var.vault_sku}"
   }
+  access_policy {
+    tenant_id = "${data.azurerm_client_config.current.tenant_id}"
+    object_id = "${data.azurerm_client_config.current.client_id}" 
+
+    key_permissions = [
+      "backup", "create", "decrypt", "delete", "encrypt", "get", "import", "list", "purge", "recover", "restore", "sign", "unwrapKey", "update", "verify","wrapKey",
+    ]
+
+    secret_permissions = [
+      "backup", "delete", "get", "list", "purge", "recover", "restore", "set",
+    ]
+  }
+
 
   network_acls {
     default_action = "Deny"
     bypass         = "AzureServices"
-    # virtual_network_subnet_ids  = "[${var.permitted_subnets}]"
+    virtual_network_subnet_ids  = ["${data.azurerm_subnet.terraform.id}"] 
   }
 
   tags {
@@ -29,9 +48,10 @@ resource "azurerm_key_vault" "vm_vault" {
 
 resource "azurerm_key_vault_key" "generated" {
   name      = "generated-certificate"
-  key_vault_id = "${azurerm_key_vault.vm_vault.id}"
+  key_vault_id = "${azurerm_key_vault.vmvault.id}"
   key_type  = "RSA"
   key_size  = 2048
 
   key_opts = "${var.key_opts}"
-}
+  depends_on = ["azurerm_key_vault.vmvault"]
+ }
