d3d8735584fe8ec22d77886b1f602b01bc3767dd
preddy727 <47993564+preddy727@users.noreply.github.com>
2019-06-26 15:05:12 -0500

    Removed from master
    Removed modules folder

--- modules/vnet/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  gateway_network_cidr = "${cidrsubnet(var.vnet_cidr, 5, 0)}"
-  application_gw_subnet_cidr = "${cidrsubnet(var.vnet_cidr, 5, 1)}"
-
-  default_subnet_cidr = "${cidrsubnet(var.vnet_cidr, 2, 2)}"
-}
