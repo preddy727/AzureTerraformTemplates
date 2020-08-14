e4d0f5562819a8f5c72205974a8442da5236fe93
Prithvi Reddy <47993564+preddy727@users.noreply.github.com>
2020-08-14 18:48:35 -0500

    Update main.tf

--- POCtoPattern/Modules/sharedimage/main.tf
+++ POCtoPattern/Modules/sharedimage/main.tf
@@ -1,3 +1,11 @@
+
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.20.0"
+  features {}
+}
+
 resource "azurerm_resource_group" "test" {
 
   name     = "example-resources"
