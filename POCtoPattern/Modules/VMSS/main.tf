bb4715b93dc056dc33f2f28de4b579ece7f1aa02
Prithvi Reddy <47993564+preddy727@users.noreply.github.com>
2020-08-14 18:46:12 -0500

    Update main.tf

--- POCtoPattern/Modules/VMSS/main.tf
+++ POCtoPattern/Modules/VMSS/main.tf
@@ -1,9 +1,9 @@
+# Configure the Azure Provider
 provider "azurerm" {
-
-  version = "~> 1.0"
-
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.20.0"
+  features {}
 }
-
 
 
 provider "template" {
