14935c4b213a53f2dc1a6bae4e63d4393921bd06
Prithvi Reddy <47993564+preddy727@users.noreply.github.com>
2020-08-14 18:46:52 -0500

    Update main.tf

--- POCtoPattern/Modules/centoshashicorpvm/main.tf
+++ POCtoPattern/Modules/centoshashicorpvm/main.tf
@@ -1,3 +1,11 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.20.0"
+  features {}
+}
+
+
 data "azurerm_subscription" "subscription" {}
 
 
